a30d363736ae319eacb6bac7b3921d25f73a9e4e
John Quaranta <john@Johns-MacBook-Air.local>
2023-05-18 18:11:51 -0500

    updated code

---
+++ Develop/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//homepage GET Route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+}); 
+
+//notes page GET Route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//GET Route for getting notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+//POST Route for a new note
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    //note id
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const allNotes = JSON.parse(data);
+        allNotes.push(newNote);
+
+    fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 2), (err) => {
+        if (err) throw err;
+        res.status(200).json(newNote);
+        });
+    });
+});
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
